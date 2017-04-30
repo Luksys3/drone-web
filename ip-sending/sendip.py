42f096a5734beb904f5740da001ced4e7f585d71
Ainoras <ainoraz@gmail.com>
2017-04-30 13:42:50 +0300

    Proper ip fix nr2

--- ip-sending/sendip.py
+++ ip-sending/sendip.py
@@ -1,20 +1,68 @@
-#!/usr/bin/python
+from pyroute2 import IPDB
 import time
-import urllib2
+from urllib2 import urlopen
+import time
 import requests
 
-def send():
-    key = 'ravem-drone'
-    url = 'http://manodarbai.eu/drone/set.php?key='+ key
-    urllib2.urlopen( url );
+ip = IPDB()
 
-while True:
+
+def send(ip_collection):
+    print "sending ip"
+    key = 'ravem-drone'
+    url = 'http://manodarbai.eu/drone/set.php?key={}&pip={}&ip={}'.format(key, ip_collection[0], ip_collection[1])
+    urlopen(url)
+
+
+def is_connected():
     try:
-        urllib2.urlopen("http://www.google.com").close()
-    except urllib2.URLError:
-        print "Not Connected"
-        time.sleep(1)
-    else:
-        print "Connected"
-        send()
-        break
+        state = ip.interfaces.wlp2s0.ipaddr[0]["address"]
+        print "Private ip is", state
+        return [True, state]
+    except:
+        print "Wifi is not connected..."
+        return [False, None]
+
+
+def check_public_ip():
+    try:
+        my_ip = urlopen('http://ip.42.pl/raw').read()
+        return my_ip
+    except:
+        print "Can't fetch public ip"
+        return None
+
+
+def ip_change():
+    while True:
+        time.sleep(3)
+        inner_ip = is_connected()
+        if inner_ip[0]:
+            ip_temp = check_public_ip()
+            if not ip_temp == None:
+                print "Returning ip"
+                return [ip_temp, inner_ip[1]]
+
+
+def run():
+    ip_collection = ip_change()
+    send(ip_collection)
+    counter = 0
+    while True:
+        if not is_connected()[0]:
+            ip_collection = ip_change()
+            print "public ip:", ip_collection[0]
+            send(ip_collection)
+        counter += 1
+        if counter == 2:
+            counter = 0
+            temp_ip = ip_change()
+            if not temp_ip[0] == ip_collection[0]:
+                ip_collection = temp_ip
+                send(ip_collection)
+        time.sleep(5)
+
+
+run()
+
+ip.release()
