20dc4b10185892d52ba1f7338a5d656a582dee66
luksys3 <theluksys3@gmail.com>
2017-04-30 17:59:57 +0300

    Update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var droneLat = 0, droneLng = 0, armedLat = 0, armedLng = 0;
 var connected = false, armed = false;
 
-var server_ip = 'http://localhost:8001';
+var server_ip = 'http://'+ document.domain +':8001';
